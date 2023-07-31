d55cd1a8b582e9151a30100ce51e55ffb22b1d5b
RakibulIslamIIT <bsse1210@iit.du.ac.bd>
2023-08-01 01:19:23 +0600

    Post Created

--- src/Components/Posts/post.jsx
+++ src/Components/Posts/post.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const post = () => {
   return (
+    <>
     <div>Eikhaen post dekhabo</div>
+    <p> eita github kaj kore kina dekhar jonno add korsi</p>
+    </>
   )
 }
 
